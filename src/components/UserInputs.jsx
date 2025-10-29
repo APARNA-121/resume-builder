e0da95fec7b71e6d386cd7884297461c6dd6fee2
APARNA-121 <aparnasreekumar121@gmail.com>
2025-10-29 12:59:10 +0530

    user input stored in state

--- src/components/UserInputs.jsx
+++ src/components/UserInputs.jsx
@@ -8,6 +8,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import { FaXmark } from "react-icons/fa6";
+import { resumeAndPrerender } from 'react-dom/static';
 
 
 const steps = ['Basic Informations', 'Contact Details', 'Education Details', 'Work Experience', 'Skills & Certifications', 'Review & Submit'];
@@ -16,6 +17,31 @@ function UserInputs() {
   const skillSuggestionArray = ['NODE JS', 'MONGODB', 'EXPRESS JS', 'REACT', 'ANGULAR', 'NEXT JS','HTML', 'CSS', 'JAVASCRIPT', 'LEADERSHIP', 'COMMUNICATION', 'MS EXCEL', 'POWERBI']
  const [activeStep, setActiveStep] = React.useState(0);
   const [skipped, setSkipped] = React.useState(new Set());
+
+  //create state for storing resume details.
+  const [resumeDetails,setResumeDetails] = React.useState({
+    username:"",
+    jobTitle:"",
+    location:"",
+    email:"",
+    mobile:"",
+    github:"",
+    linkedin:"",
+    portfolio:"",
+    course:"",
+    college:"",
+    university:"",
+    passoutYear:"",
+    jobType:"",
+    company:"",
+    clocation:"",
+    duration:"",
+    userSkills:[],
+    summary:""
+  })
+
+  console.log(resumeDetails);
+  
 
   const isStepOptional = (step) => {
     return step === 1;
@@ -65,9 +91,9 @@ function UserInputs() {
             <div>
                 <h3>Personal Details</h3>
                 <div className='row p-3'>
-                     <TextField id="standard-basic-name" label="Full Name" variant="standard" />
-                     <TextField id="standard-basic-job" label="Job Title" variant="standard" />
-                     <TextField id="standard-basic-location" label="Location" variant="standard" />
+                     <TextField value={resumeDetails.username} onChange={e=>setResumeDetails({...resumeDetails,username:e.target.value})} id="standard-basic-name" label="Full Name" variant="standard" />
+                     <TextField  value={resumeDetails.jobTitle} onChange={e=>setResumeDetails({...resumeDetails,jobTitle :e.target.value})}  id="standard-basic-job" label="Job Title" variant="standard" />
+                     <TextField  value={resumeDetails.location} onChange={e=>setResumeDetails({...resumeDetails,location:e.target.value})}  id="standard-basic-location" label="Location" variant="standard" />
                 </div>
             </div>
         )
@@ -75,11 +101,11 @@ function UserInputs() {
             <div>
                 <h3>Contact Details</h3>
                 <div className='row p-3'>
-                     <TextField id="standard-basic-email" label="Email" variant="standard" />
-                     <TextField id="standard-basic-phone" label="Phone Number" variant="standard" />
-                     <TextField id="standard-basic-github" label="Github Profile Link" variant="standard" />
-                      <TextField id="standard-basic-linkedin" label="Linkedin Profile Link" variant="standard" />
-                      <TextField id="standard-basic-portfolio" label="Portfolio Link" variant="standard" />
+                     <TextField  value={resumeDetails.email} onChange={e=>setResumeDetails({...resumeDetails,email:e.target.value})} id="standard-basic-email" label="Email" variant="standard" />
+                     <TextField  value={resumeDetails.mobile} onChange={e=>setResumeDetails({...resumeDetails,mobile:e.target.value})}id="standard-basic-phone" label="Phone Number" variant="standard" />
+                     <TextField value={resumeDetails.github} onChange={e=>setResumeDetails({...resumeDetails,github:e.target.value})}id="standard-basic-github" label="Github Profile Link" variant="standard" />
+                      <TextField  value={resumeDetails.linkedin} onChange={e=>setResumeDetails({...resumeDetails,linkedin:e.target.value})}id="standard-basic-linkedin" label="Linkedin Profile Link" variant="standard" />
+                      <TextField value={resumeDetails.portfolio} onChange={e=>setResumeDetails({...resumeDetails,portfolio:e.target.value})}id="standard-basic-portfolio" label="Portfolio Link" variant="standard" />
                 </div>
             </div>
         )
@@ -87,10 +113,10 @@ function UserInputs() {
             <div>
                 <h3>Education Details</h3>
                 <div className='row p-3'>
-                     <TextField id="standard-basic-course" label="Course Name" variant="standard" />
-                     <TextField id="standard-basic-college" label="College Name" variant="standard" />
-                     <TextField id="standard-basic-university" label="University" variant="standard" />
-                      <TextField id="standard-basic-passout" label="Year of Passout" variant="standard" />
+                     <TextField  value={resumeDetails.course} onChange={e=>setResumeDetails({...resumeDetails,course:e.target.value})}  id="standard-basic-course" label="Course Name" variant="standard" />
+                     <TextField  value={resumeDetails.college} onChange={e=>setResumeDetails({...resumeDetails,college:e.target.value})}  id="standard-basic-college" label="College Name" variant="standard" />
+                     <TextField  value={resumeDetails.university} onChange={e=>setResumeDetails({...resumeDetails,university:e.target.value})}  id="standard-basic-university" label="University" variant="standard" />
+                      <TextField  value={resumeDetails.passoutYear} onChange={e=>setResumeDetails({...resumeDetails,passoutYear:e.target.value})}  id="standard-basic-passout" label="Year of Passout" variant="standard" />
                 </div>
             </div>
         )
@@ -98,10 +124,10 @@ function UserInputs() {
             <div>
                 <h3>Professional Details</h3>
                 <div className='row p-3'>
-                     <TextField id="standard-basic-internship" label="Job or Internship" variant="standard" />
-                     <TextField id="standard-basic-company" label="Company Name" variant="standard" />
-                     <TextField id="standard-basic-Cmpnylocation" label="Company Location" variant="standard" />
-                     <TextField id="standard-basic-duration" label="Duration" variant="standard" />
+                     <TextField  value={resumeDetails.jobType} onChange={e=>setResumeDetails({...resumeDetails,jobType:e.target.value})}  id="standard-basic-internship" label="Job or Internship" variant="standard" />
+                     <TextField  value={resumeDetails.company} onChange={e=>setResumeDetails({...resumeDetails,company:e.target.value})}  id="standard-basic-company" label="Company Name" variant="standard" />
+                     <TextField  value={resumeDetails.clocation} onChange={e=>setResumeDetails({...resumeDetails,clocation:e.target.value})}  id="standard-basic-Cmpnylocation" label="Company Location" variant="standard" />
+                     <TextField  value={resumeDetails.duration} onChange={e=>setResumeDetails({...resumeDetails,duration:e.target.value})}  id="standard-basic-duration" label="Duration" variant="standard" />
                 </div>
             </div>
         )
--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,8 +1,29 @@
+import { Box, Paper } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { MdDelete } from "react-icons/md";
 
 function History() {
   return (
-    <div>History</div>
+    <div>
+      <h1 className='text-center my-5'>Downloaded Resume History</h1>
+      <Link to={'/resume'} className='float-end me-5' style={{marginTop:'-80px'}}>Back</Link>
+      <Box component="section" className='container-fluid'>
+      <div className="row">
+      <div className="col-md-4">
+        <Paper elevation={3} sx={{my:5, p:5, textAlign:'center'}}>
+          <div className="d-flex justify-content-between align-items-center">
+            <h6>Review At: date&time </h6>
+            <button className='btn text-danger fs-4'><MdDelete /></button>
+          </div>
+          <div className="border rounded p-3">
+            <img width={'200px'} height={'200px'} src="https://www.my-resume-templates.com/wp-content/uploads/2024/03/standard-resume-format-template-word-1-307.jpg" alt="resume" />
+          </div>
+        </Paper>
+      </div>
+      </div>
+    </Box>
+    </div>
   )
 }
 
